ad2c1ad7394a93b2845d2e114790ff49e0e7b3e1
Sahasrabda <78862847+Sahasrabda@users.noreply.github.com>
2021-02-19 07:33:09 +0530

    Add files via upload

---
+++ Week2Q4.CPP
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+    int arr[10],i,sma,lar;
+    printf("Enter the array: \n");
+    for(i=0;i<10;i++){
+        scanf("%d", &arr[i]);
+    }
+    sma=arr[0],lar=arr[0];
+    for(i=0;i<10;i++){
+        if(arr[i]<sma){
+            sma=arr[i];
+        }
+        if(arr[i]>lar){
+            lar=arr[i];
+        }
+
+    }printf("\n The largest number in the array is : %d", lar);
+        printf("\n The smallest number in the array is : %d", sma);
+   return 0;
+}
