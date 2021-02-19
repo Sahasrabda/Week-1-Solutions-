570a89744d570f046b74f908ed99f11aaf68b333
Sahasrabda <78862847+Sahasrabda@users.noreply.github.com>
2021-02-19 07:32:03 +0530

    Add files via upload

---
+++ Week2Q3.CPP
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<stdio.h>
+#include<string.h>
+using namespace std;
+int main()
+{
+    char sent[100];
+    int words=0,i;
+    printf("Enter a sentence: \n");
+    scanf("%[^\n]",sent);
+
+    for(i=0; sent[i]!='\0'; i++){
+        if(sent[i]== ' '){
+            words++;
+        }
+    }
+    printf("Number of words in the sentence is %d", words+1);
+    return 0;
+}
